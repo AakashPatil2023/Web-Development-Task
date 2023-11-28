01e492453fe71323a6eec5044e85f95eb967759a
Aakash Patil <112317129+AakashPatil2023@users.noreply.github.com>
2023-11-28 11:43:56 +0530

    Update dbconnection.py

--- dbconnection.py
+++ dbconnection.py
@@ -17,7 +17,7 @@ file = form['file']
 # file = up_file.file.getvalue()
 password = form['password'].value
 
-query = "INSERT INTO form (`Partner`, `Name`, `Email`, `Number`, `Pan`, `Pin Code`, `KYC`, `file`, `password`) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+query = "INSERT INTO form (Partner, Name, Email, Number, Pan, Pin Code, KYC, file, password) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 values = (option, name, email, number, pan, pincode, kyc, file.value, password)
 
 cursor.execute(query, values)
