bf60f0a36e6b49db831036c1fed9e8c99e47928d
Aakash Patil <112317129+AakashPatil2023@users.noreply.github.com>
2023-12-01 13:09:21 +0530

    Update dbconnection.py

--- dbconnection.py
+++ dbconnection.py
@@ -1,24 +1,27 @@
 import cgi
+import cgitb
 import mysql.connector
+import sys
+
+cgitb.enable()
 
 try:
-    conn = mysql.connector.connect(host='localhost', username='root', password='root', database='form_db')
+    conn = mysql.connector.connect(host='localhost', user='root', password='root', database='form_db')
     cursor = conn.cursor()
 
-    form = cgi.FieldStorage()
+    form = cgi.FieldStorage(environ={'REQUEST_METHOD': 'POST'})
     option = form.getvalue("SelectPartner")
     name = form.getvalue("name")
     email = form.getvalue("email")
     number = form.getvalue("number")
     pan = form.getvalue("pan")
     pincode = form.getvalue("pincode")
-    kyc = form.getvalue("Skyc")
-    file = form.getvalue("file")
+    kyc = form.getvalue("kyc") 
+    file = form.getvalue("file") 
     password = form.getvalue("password")
 
-    query = "INSERT INTO form (Partner, Name, Email, Number, Pan, PinCode, KYC, file, password) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    values = (option, name, email, number, pan, pincode, kyc, file, password)
-
+    query = "INSERT INTO form (Partner, Name, Email, Number, Pan, PinCode, KYC, Password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (option, name, email, number, pan, pincode, kyc, file, password)  
     cursor.execute(query, values)
 
     conn.commit()
@@ -27,6 +30,6 @@ except mysql.connector.Error as error:
     print("Failed to insert into MySQL table {}".format(error))
 
 finally:
-    if (conn).is_connected():
+    if conn.is_connected():
         cursor.close()
         conn.close()
